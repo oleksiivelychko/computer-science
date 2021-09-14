9ec512ec4f16ea714efeb2f551d1ab79a360add1
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-09-14 14:13:31 +0300

    change function signature

--- quick_sort/quick_sort.go
+++ quick_sort/quick_sort.go
@@ -10,21 +10,21 @@ Worst case (pivot is the first) is O(n) * O(n) = O(n^2)
 2. Compare items - all items less than pivot move before it, all items greater than pivot move after it.
 3. Repeat steps 1,2 towards arrays - left and right between pivot.
 */
-func QuickSort(slice []uint) []uint {
+func QuickSort(slice []int) []int {
 	if len(slice) <= 1 {
 		return slice
 	}
 
 	pivot := slice[0]
 
-	var head []uint
+	var head []int
 	for _, num := range slice[1:] {
 		if num <= pivot {
 			head = append(head, num)
 		}
 	}
 
-	var tail []uint
+	var tail []int
 	for _, num := range slice[1:] {
 		if num > pivot {
 			tail = append(tail, num)
--- quick_sort/quick_sort_test.go
+++ quick_sort/quick_sort_test.go
@@ -4,21 +4,18 @@ import (
 	"testing"
 )
 
-var array = [4]uint{10, 5, 2, 3}
+var array = [3]int{21, -1, 11}
 
 func TestQuickSort(t *testing.T) {
 	var sorted = QuickSort(array[:])
-	if sorted[0] != 2 {
-		t.Errorf("[func QuickSort(slice []uint) []uint] -> %d != 2", sorted[0])
+	if sorted[0] != -1 {
+		t.Errorf("[func QuickSort(slice []int) []int] -> %d != -1", sorted[0])
 	}
-	if sorted[1] != 3 {
-		t.Errorf("[func QuickSort(slice []uint) []uint] -> %d != 3", sorted[1])
+	if sorted[1] != 11 {
+		t.Errorf("[func QuickSort(slice []int) []int] -> %d != 11", sorted[1])
 	}
-	if sorted[2] != 5 {
-		t.Errorf("[func QuickSort(slice []uint) []uint] -> %d != 5", sorted[2])
-	}
-	if sorted[3] != 10 {
-		t.Errorf("[func QuickSort(slice []uint) []uint] -> %d != 10", sorted[3])
+	if sorted[2] != 21 {
+		t.Errorf("[func QuickSort(slice []int) []int] -> %d != 21", sorted[2])
 	}
 }
 
